61a704ccdefecfc619d92560ef938d0241931598
Rikii-dev <rmb0007@alu.medac.es>
2025-05-16 10:53:00 +0200

    Primera clase y segunda hechas

---
+++ PracticaTeoremaPitagoras/src/app/Principal2.java
@@ -0,0 +1,19 @@
+package app;
+
+import clases.TrianguloRectangulo;
+
+public class Principal2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double areaConValores, areaPorDefecto, perimetroConValores, perimetroPorDefecto;
+		TrianguloRectangulo convalores= new TrianguloRectangulo(5,4);
+		TrianguloRectangulo pordefecto= new TrianguloRectangulo();
+		areaConValores= convalores.area();
+		areaPorDefecto= pordefecto.area();
+		perimetroConValores= convalores.perimetro();
+		perimetroPorDefecto=pordefecto.perimetro();
+		
+	}
+
+}
---
+++ PracticaTeoremaPitagoras/src/app/principal.java
@@ -0,0 +1,10 @@
+package app;
+
+import clases.TrianguloRectangulo;
+
+public class principal {
+
+	public static void main(String[] args) {
+	}
+
+}
